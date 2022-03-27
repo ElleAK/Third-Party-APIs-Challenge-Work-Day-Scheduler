b73643e82376ce2251ae3f23e56eb5c179e5ee5e
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-03-26 22:01:48 -0400

    color coded time

--- Develop/script.js
+++ Develop/script.js
@@ -6,4 +6,23 @@ $(document).ready(function(){
 $("#currentDay").text(moment().format("MMMM Do YYYY, hh:mm:ss a"));
 
 
+$(".time-block").each(function(){
+    var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+    var currentTime = moment().hours();
+
+    if (currentTime > hourBlock) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    } else if (currentTime < hourBlock) {
+        $(this).addClass("future");
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+    } else {
+        $(this).removeClass("future");
+        $(this).addClass("present");
+        $(this).removeClass("past");
+    }
+})
+
 })
