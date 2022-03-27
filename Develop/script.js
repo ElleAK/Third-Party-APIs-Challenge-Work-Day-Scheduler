e5647abb0bf97cba9dafaa4fc42f72558cf23047
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-03-26 23:16:38 -0400

    save button local storage

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,9 @@
 
 $(document).ready(function(){
 
+
+    
+
 $("#currentDay").text(moment().format("MMMM Do YYYY, hh:mm:ss a"));
 
 
@@ -24,5 +27,29 @@ $(".time-block").each(function(){
         $(this).removeClass("past");
     }
 })
+
+$(".saveBtn").on("click", function () {
+    //get nearby values.
+    
+    var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+    var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+
+    //set items in local storage.
+    localStorage.setItem(time, text);
+})
+
+//var saveEvent = JSON.parse(localStorage.getItem("hour"));
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
 
 })
